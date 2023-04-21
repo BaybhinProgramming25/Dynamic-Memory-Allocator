95cabceb373a614656e15d7effb44b881ad8b8e5
Baybhin <baybhin@gmail.com>
2023-04-20 23:24:30 -0400

    Completed Commit

---
+++ TParser/src/MinimalClassObject.py
@@ -0,0 +1,7 @@
+class SimpleClassObject:
+    def __init__(self, courseName, credits, grade, term, year):
+        self.courseName = courseName
+        self.credits = credits
+        self.grade = grade
+        self.term = term
+        self.year = year
